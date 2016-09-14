4fad613479a65df049aa67a45d0167b57ca69e88
235 <hotup@qq.com>
2016-09-14 20:24:19 +0800

    ensure SXor inline on flash

--- tools/mem/obs/SXor.hx
+++ tools/mem/obs/SXor.hx
@@ -1,160 +1,97 @@
 package mem.obs;
 
-
-#if macro
-import haxe.macro.Type;
-import haxe.macro.Expr;
-import haxe.macro.Context;
-import haxe.macro.PositionTools.here;
-import haxe.crypto.Md5;
-import haxe.io.Bytes;
-import haxe.Int64;
-import mem.Ut;
-import mem.obs.SxIntBuild;
-
-/**
-* 构建一个 256 字节的数据字段, 在生成后将为固定的字节, 或许未来将从网络上获得生成好的 256 字节 + 8个Int 的明码.
-*/
-class SxBuild{
-	static inline var SX_MAX = 256;
-	public static function make(dk:String){
-		var dv:String = Context.definedValue(dk);
-		if (dv == null || dv == "" || dv == "1") Context.error("Must provide -D " + dk + "=passwd", here());
-		var keys = Md5.make(Bytes.ofString(dv));
-		var bytes = Bytes.alloc(SX_MAX);
-
-		// 随机填充数据
-		for (i in 0...SX_MAX) bytes.set(i, Ut.rand(SX_MAX, 1));
-
-
-		// 随机的位置, 不过最终这个值还是固定值, 一次性生成 12 个, 后 8 位用于存放地址的地址
-		var sx:Array<Int> = [];
-		for(i in 1...(SX_MAX >> 2) - 1){
-			sx.push(i << 2);
-		}
-		Ut.shuffle(sx, 5);
-		sx = sx.slice(0, 12);
-		Ut.shuffle(sx, 2);
-
-		var i64s:Array<haxe.Int64> = [];
-		for (i in 0...4) {
-			i64s[i] = SxIntBuild.make(sx[i], i);			// Int64(位置)
-			//bytes.setInt32(sx[i], keys.getInt32(i << 2)); // error ????
-			for (j in 0...4) {
-				bytes.set(sx[i] + j, keys.get((i << 2) + j));					// 将 key  值写入
-				bytes.set(sx[4 + i] + j , (i64s[i].low >> (j * 8)) & 0xFF);		// 将 low  值写入
-				bytes.set(sx[8 + i] + j , (i64s[i].high >> (j * 8)) & 0xFF);	// 将 hight值写入
-			}
-		}
-		//trace(i64s[0].low == bytes.getInt32(sx[4 + 0]) &&  i64s[0].high == bytes.getInt32(sx[8 + 0]));
-		//trace(i64s[3].low == bytes.getInt32(sx[4 + 3]) &&  i64s[3].high == bytes.getInt32(sx[8 + 3]));
-		//trace(sx.toString() + ", md5: " + keys.toHex());
-
-		var fields = Context.getBuildFields();
-		var codes:Array<Expr> = [];
-		if (Context.defined("flash10_3")){
-			// 仅在 flash 这样初使化就行了, 但由于在 FFunc({expr: 处, 无法简单使用 #if flash 来添加这些
-			var chunk = SX_MAX >> 2;	// (256/4) = 64
-			for (i in 0...chunk) codes.push( macro Memory.setI32(sa.c_ptr + $v{i << 2}, $v{bytes.getInt32($v{i << 2})}) );
-			Ut.shuffle(codes, 3);
-		}else{
-			codes.push(macro {
-				var hex = $v{bytes.toHex()};
-				var i = 0;
-				while (i < $v{SX_MAX << 1}){ // 256 * 2
-					Memory.setByte(sa.c_ptr + (i >> 1), Std.parseInt("0x" + hex.charAt(i) + hex.charAt(i + 1)));
-					i += 2;
-				}
-			});
-		}
-
-		var pos = here();
-		fields.push({
-			name: "init",
-			access: [APublic, AStatic, AInline],
-			meta:[{name: ":dce", pos: pos}],
-			pos: pos,
-			kind: FFun({
-				ret: macro :Void,
-				args:[],
-				expr: macro {
-					if (@:privateAccess Ram.current == null) throw "Need to init ApplicationDomain.currentDomain.domainMemory";
-					if (sa != mem.Malloc.NUL) return;
-					sa = new SxKeys($v{SX_MAX});
-					$b{codes};
-					x_0 = SxInt.fromInt2(Memory.getI32(sa.c_ptr + $v{sx[4 + 0]}), Memory.getI32(sa.c_ptr + $v{sx[8 + 0]}));
-					x_1 = SxInt.fromInt2(Memory.getI32(sa.c_ptr + $v{sx[4 + 1]}), Memory.getI32(sa.c_ptr + $v{sx[8 + 1]}));
-					x_2 = SxInt.fromInt2(Memory.getI32(sa.c_ptr + $v{sx[4 + 2]}), Memory.getI32(sa.c_ptr + $v{sx[8 + 2]}));
-					x_3 = SxInt.fromInt2(Memory.getI32(sa.c_ptr + $v{sx[4 + 3]}), Memory.getI32(sa.c_ptr + $v{sx[8 + 3]}));
-				}
-			})
-		});
-
-		return fields;
-	}
-}
-
-#else
-
 import mem.Ptr;
-import mem.obs.SxInt;
-import mem.obs.SxIntBuild;
+import mem.obs.SxInt.*;
+import mem.Ptr.Memory.setI32;
+import mem.Ptr.Memory.getI32;
+/**
+* 每个 x_n 的值为 8字节的明文, 通过这个明文计算将得到一个密钥块(4字节)的"位置值"
+*/
+#if !macro
+@:build(mem.obs.SxInt.SXorBuild.make("sx-pwd"))
+#end
+class SXor {
+	static var x_0: AU8;
+	static var x_1: AU8;
+	static var x_2: AU8;
+	static var x_3: AU8;
+	static var  sa: Ptr = Malloc.NUL;
 
-@:build(mem.obs.SXor.SxBuild.make("sx-pwd"))
-@:dce class SXor{
-	static var x_0:SxInt;
-	static var x_1:SxInt;
-	static var x_2:SxInt;
-	static var x_3:SxInt;
-	static var sa:SxKeys = mem.Malloc.NUL;
+	public static function create(low:Int, high:Int):AU8 {
+		var r:AU8 = cast mem.Malloc.make(8, false);
+	#if !neko
+		setI32(r, low);
+		setI32(r + 4, high);
+	#else
+		r[0] = low & 0xff;
+		r[1] = ( low >>  8) & 0xff;
+		r[2] = ( low >> 16) & 0xff;
+		r[3] = ( low >>>24) & 0xff;
+		r[4] = high & 0xff;
+		r[5] = (high >>  8) & 0xff;
+		r[6] = (high >> 16) & 0xff;
+		r[7] = (high >>>24) & 0xff;
+	#end
+		return r;
+	}
 
-	public static inline function make(src:Ptr, len:Int, dst:Ptr):Void{
-		//if (dst == -1)
-			//dst = src;
-		//else if (dst > src && dst < src + len)
-			//throw "It will be overwritten";
+	public static inline function make(src:Ptr, len:Int, dst:Ptr):Void {
 		var offset = 0;
-		var b4 = len - (len % 4);
-		var mod = 0;
+		var bmod = 0;
+		var p:Int = sa;
 	#if !neko	// Uncaught exception $sset on neko
-		while (b4 > offset){
-			mod = offset >> 2;
-			switch(mod % 4){
-			case 0: Memory.setI32(dst + offset, Memory.getI32(src + offset) ^ Memory.getI32( sa.c_ptr + x_0.calc_0() ));
-			case 1: Memory.setI32(dst + offset, Memory.getI32(src + offset) ^ Memory.getI32( sa.c_ptr + x_1.calc_1() ));
-			case 2: Memory.setI32(dst + offset, Memory.getI32(src + offset) ^ Memory.getI32( sa.c_ptr + x_2.calc_2() ));
-			default:Memory.setI32(dst + offset, Memory.getI32(src + offset) ^ Memory.getI32( sa.c_ptr + x_3.calc_3() ));
+		var b4 = len - (len & (4 - 1));
+		var mod = 0;
+		while (b4 > offset) {
+			switch (mod) {
+			case 0: setI32(dst + offset, getI32(src + offset) ^ getI32( p + c0(x_0) ));
+			case 1: setI32(dst + offset, getI32(src + offset) ^ getI32( p + c1(x_1) ));
+			case 2: setI32(dst + offset, getI32(src + offset) ^ getI32( p + c2(x_2) ));
+			default:setI32(dst + offset, getI32(src + offset) ^ getI32( p + c3(x_3) ));
 			}
-			offset += 4;
+		offset += 4;
+		mod    += 1;
+		mod = mod & (4 - 1);
+		}
+
+		while (bmod < 3) {
+			switch (mod) {
+			case 0:  Memory.setByte(dst + offset, Memory.getByte(src + offset) ^ Memory.getByte(p + bmod + c0(x_0)));
+			case 1:  Memory.setByte(dst + offset, Memory.getByte(src + offset) ^ Memory.getByte(p + bmod + c1(x_1)));
+			case 2:  Memory.setByte(dst + offset, Memory.getByte(src + offset) ^ Memory.getByte(p + bmod + c2(x_2)));
+			default: Memory.setByte(dst + offset, Memory.getByte(src + offset) ^ Memory.getByte(p + bmod + c3(x_3)));
+			}
+		++offset;
+		++bmod;
+		}
+	#else
+		while (offset < len) {
+			bmod = bmod & (4 - 1);
+			switch((offset >> 2) & (4 - 1)) {
+			case 0:  Memory.setByte(dst + offset, Memory.getByte(src + offset) ^ Memory.getByte(p + bmod + c0(x_0)));
+			case 1:  Memory.setByte(dst + offset, Memory.getByte(src + offset) ^ Memory.getByte(p + bmod + c1(x_1)));
+			case 2:  Memory.setByte(dst + offset, Memory.getByte(src + offset) ^ Memory.getByte(p + bmod + c2(x_2)));
+			default: Memory.setByte(dst + offset, Memory.getByte(src + offset) ^ Memory.getByte(p + bmod + c3(x_3)));
+			}
+		++offset;
+		++bmod;
 		}
 	#end
-		while (len > offset) {
-			mod = offset % 16;
-			switch(mod >> 2){
-			case 0: Memory.setByte(dst + offset, Memory.getByte(src + offset) ^ Memory.getByte((mod % 4) + sa.c_ptr + x_0.calc_0() ));
-			case 1: Memory.setByte(dst + offset, Memory.getByte(src + offset) ^ Memory.getByte((mod % 4) + sa.c_ptr + x_1.calc_1() ));
-			case 2: Memory.setByte(dst + offset, Memory.getByte(src + offset) ^ Memory.getByte((mod % 4) + sa.c_ptr + x_2.calc_2() ));
-			default:Memory.setByte(dst + offset, Memory.getByte(src + offset) ^ Memory.getByte((mod % 4) + sa.c_ptr + x_3.calc_3() ));
-			}
-			offset += 1;
-		}
 	}
 
-	public static inline function test(){
+	@:dce public static inline function test(){
 		var t = haxe.io.Bytes.alloc(16);
-		var p0 = x_0.calc_0();
-		var p1 = x_1.calc_1();
-		var p2 = x_2.calc_2();
-		var p3 = x_3.calc_3();
+		var p0 = c0(x_0);
+		var p1 = c1(x_1);
+		var p2 = c2(x_2);
+		var p3 = c3(x_3);
 		trace([p0, p1, p2, p3]);
 	#if flash
-		Ram.readBytes(sa.c_ptr + p0, 4, t.getData());
-		Ram.readBytes(sa.c_ptr + p1, 4, t.getData());
-		Ram.readBytes(sa.c_ptr + p2, 4, t.getData());
-		Ram.readBytes(sa.c_ptr + p3, 4, t.getData());
+		Ram.readBytes(sa + p0, 4, t.getData());
+		Ram.readBytes(sa + p1, 4, t.getData());
+		Ram.readBytes(sa + p2, 4, t.getData());
+		Ram.readBytes(sa + p3, 4, t.getData());
 		trace(t.toHex());
 	#end
 	}
-
 }
-#end
--- tools/mem/obs/SxInt.hx
+++ tools/mem/obs/SxInt.hx
@@ -1,66 +1,137 @@
 package mem.obs;
 
+
+#if macro
+import haxe.macro.Type;
+import haxe.macro.Expr;
+import haxe.macro.Context;
+import haxe.macro.PositionTools.here;
+import haxe.crypto.Md5;
+import haxe.io.Bytes;
 import haxe.Int64;
-import mem.Ptr;
-import mem.obs.SXor;
+import mem.Ut;
+@:dce class SXorBuild {
+	static function vx(v: Int/*uint8_t*/): Array<Int> {
+		var va = [0, 0, 0, 0, 0, 0, 0, 0];
 
-#if !macro
-@:build(mem.Struct.StructBuild.make())
-#end
-@:dce abstract SxKeys(Ptr) from Ptr{
-	public inline function new(len:Int) {
-		this = Malloc.make(len, false);
+		va[0] = v & 0xFF;
+
+		va[7] = mem.Ut.rand(0xFF, 1);
+		va[0] ^= va[7];
+
+		// (~45 & 0xFF) == 210
+		// ((100 & 210) | (100 & 45)) == 100
+		va[6] = mem.Ut.rand(0xFF, 1);
+		va[5] = va[0] & va[6];
+		va[0] &= ~va[6] & 255;
+		va[0] ^= va[5];
+
+		va[4] = mem.Ut.rand(0xFF, 1);
+		va[3] = va[0] & va[4];
+		va[0] &= ~va[4] & 255;
+		va[0] ^= va[3];
+
+		va[2] = mem.Ut.rand(0xFF, 1);
+		va[1] = va[0] & va[2];
+		va[0] &= ~va[2] & 255;
+		va[0] ^= va[1];
+		return va;
 	}
-	public var c_ptr(get, never):Ptr;
-	inline function get_c_ptr():Ptr return this;
+	// 一个 0x04 ~ 0xF7 的数(必须为4的整倍)用于分段保存密钥的地址
+	static function createSxInt(v: Int/*uint8_t*/, mod = 0): haxe.Int64{
+		var va = vx(v);
+		switch (mod) {//(va[4] | va[5] << 8 | va[6] << 16 | va[7] << 24, va[0] | va[1] << 8 | va[2] << 16 | va[3] << 24)
+			case 0:  // 4~7, 3~6, 0~5, 1~2
+				return haxe.Int64.make(va[7] | va[0] << 8 | va[3] << 16 | va[4] << 24, va[5] | va[2] << 8 | va[1] << 16 | va[6] << 24);
+			case 1:  // 0~7, 2~5, 3~4, 1~6
+				return haxe.Int64.make(va[3] | va[2] << 8 | va[1] << 16 | va[0] << 24, va[7] | va[6] << 8 | va[5] << 16 | va[4] << 24);
+			case 2:  // 0~4, 1~7, 2~6, 3~5
+				return haxe.Int64.make(va[0] | va[3] << 8 | va[2] << 16 | va[1] << 24, va[4] | va[7] << 8 | va[6] << 16 | va[5] << 24);
+			case 3:  // 4~5, 6~7, 0~1, 2~3
+				return haxe.Int64.make(va[5] | va[4] << 8 | va[7] << 16 | va[6] << 24, va[1] | va[0] << 8 | va[3] << 16 | va[2] << 24);
+			case _:
+				throw "???";
+		}
+	}
+
+	public static function make(dk:String){
+		var dv:String = Context.definedValue(dk);
+		if (dv == null || dv == "" || dv == "1") Context.error("Must provide -D " + dk + "=passwd", here());
+		var keys = Md5.make(Bytes.ofString(dv));
+
+		// 随机数据串, 最后它将 ==  密钥(16字节) + (4 * SxInt(8字节每个)) + (rand_uint8 * REST)
+		var bytes = Bytes.alloc(SX_MAX);
+		// 值随机为 [0x4, 0xF7],
+		for (i in 0...SX_MAX) bytes.set(i, Ut.rand(SX_MAX - 8, 4) );
+
+
+		// 密钥将要存放的位置, 位置是随机的 确保其为 4 的整倍数
+		var cx:Array<Int> = [];
+		for(i in 1...(SX_MAX >> 2) - 1){ // i in [1, 63) if SX_MAX == 256
+			cx.push(i << 2);
+		}
+		Ut.shuffle(cx, 5);
+		cx = cx.slice(0, 12);
+		Ut.shuffle(cx, 2);
+
+		var i64s:Array<haxe.Int64> = [];
+		for (i in 0...4) {
+			i64s[i] = createSxInt(cx[i], i);
+			for (j in 0...4) {
+				bytes.set(cx[i + 0] + j , keys.get((i << 2) + j));
+				bytes.set(cx[4 + i] + j , (i64s[i].low >> (j * 8)) & 0xFF);
+				bytes.set(cx[8 + i] + j , (i64s[i].high >> (j * 8)) & 0xFF);
+			}
+		}
+
+		var fields = Context.getBuildFields();
+
+		var codes:Array<Expr> = [];
+
+		if (Context.defined("flash10_3")) {
+			var chunk = SX_MAX >> 2;	// (256/4) = 64
+			for (i in 0...chunk)
+				codes.push( macro Memory.setI32(p + $v{i << 2}, $v{bytes.getInt32($v{i << 2})}) );
+			Ut.shuffle(codes, 3);
+		}else{
+			codes.push(macro {
+				var hex = $v{bytes.toHex()};
+				var i = 0;
+				while (i < $v{SX_MAX << 1}){ // 256 * 2
+					Memory.setByte(p + (i >> 1), Std.parseInt("0x" + hex.charAt(i) + hex.charAt(i + 1)));
+					i += 2;
+				}
+			});
+		}
+
+		var pos = here();
+		fields.push({
+			name: "init",
+			access: [APublic, AStatic],
+			meta:[{name: ":analyzer", pos: pos, params:[macro no_const_propagation]}],
+			pos: pos,
+			kind: FFun({
+				ret: macro :Void,
+				args:[],
+				expr: macro {
+					if (@:privateAccess Ram.current == null) throw "Ram.current is null";
+					if (sa != mem.Malloc.NUL) return;
+					sa = Malloc.make($v { SX_MAX }, false);
+					var p:Int = cast sa;
+					$a{codes};
+					x_0 = create(Memory.getI32(p + $v{cx[4 + 0]}), Memory.getI32(p + $v{cx[8 + 0]}));
+					x_1 = create(Memory.getI32(p + $v{cx[4 + 1]}), Memory.getI32(p + $v{cx[8 + 1]}));
+					x_2 = create(Memory.getI32(p + $v{cx[4 + 2]}), Memory.getI32(p + $v{cx[8 + 2]}));
+					x_3 = create(Memory.getI32(p + $v{cx[4 + 3]}), Memory.getI32(p + $v{cx[8 + 3]}));
+				}
+			})
+		});
+
+		return fields;
+	}
+	static inline var SX_MAX = 256;
 }
-
-
-#if !macro
-@:build(mem.Struct.StructBuild.make())
 #end
-abstract SxInt(Ptr) from Ptr{
-	@idx(1) var v_0:Int;
-	@idx(1) var v_1:Int;
-	@idx(1) var v_2:Int;
-	@idx(1) var v_3:Int;
-	@idx(4, -4) var low:Int;
-	@idx(1) var v_4:Int;
-	@idx(1) var v_5:Int;
-	@idx(1) var v_6:Int;
-	@idx(1) var v_7:Int;
-	@idx(4, -4) var high:Int;
-
-	@:dce public inline  function calc_0():Int return v_5 ^ v_2 | (v_1 & v_2) ^ v_6 | (v_7 & v_6) ^ v_0 | (v_3 & v_0) ^ v_4;
-
-	@:dce public inline  function calc_1():Int return v_7 ^ v_6 | (v_5 & v_6) ^ v_4 | (v_3 & v_4) ^ v_2 | (v_1 & v_2) ^ v_0;
-
-	@:dce public inline  function calc_2():Int return v_4 ^ v_7 | (v_6 & v_7) ^ v_5 | (v_0 & v_5) ^ v_3 | (v_2 & v_3) ^ v_1;
-
-	@:dce public inline  function calc_3():Int return v_1 ^ v_0 | (v_3 & v_0) ^ v_2 | (v_5 & v_2) ^ v_4 | (v_7 & v_4) ^ v_6;
-
-	@:dce public static inline function fromInt64(v:Int64):SxInt{
-		return fromInt2(v.low, v.high);
-	}
-
-	@:dce public static inline function fromInt2(l:Int, h:Int):SxInt{
-		var x = new SxInt();
-	#if !neko
-		x.low = l;
-		x.high = h;
-	#else
-		x.v_0 = l & 0xff;
-		x.v_1 = (l >> 8) & 0xff;
-		x.v_2 = (l >> 16) & 0xff;
-		x.v_3 = (l >>> 24) & 0xff;
-		x.v_4 = h & 0xff;
-		x.v_5 = (h >> 8) & 0xff;
-		x.v_6 = (h >> 16) & 0xff;
-		x.v_7 = (h >>> 24) & 0xff;
-	#end
-		return x;
-	}
-}
 
 // 0 --- 4~7,3~6,0~5,1~2
 //// v_0 ^ v_1 | (v_2 & v_1) ^ v_3 | (v_4 & v_3) ^ v_5 | (v_6 & v_5) ^ v_7
@@ -77,3 +148,17 @@ abstract SxInt(Ptr) from Ptr{
 // 3 --- 4~5, 6~7, 0~1, 2~3
 //// v_0 ^ v_1 | (v_2 & v_1) ^ v_3 | (v_4 & v_3) ^ v_5 | (v_6 & v_5) ^ v_7
 //   v_1 ^ v_0 | (v_3 & v_0) ^ v_2 | (v_5 & v_2) ^ v_4 | (v_7 & v_4) ^ v_6;
+
+@:dce class SxInt {
+  #if (macro || flash)
+	macro public static function c0(v) return macro untyped (__vmem_get__(0, $v + 5) ^ __vmem_get__(0, $v + 2) | (__vmem_get__(0, $v + 1) & __vmem_get__(0, $v + 2)) ^ __vmem_get__(0, $v + 6) | (__vmem_get__(0, $v + 7) & __vmem_get__(0, $v + 6)) ^ __vmem_get__(0, $v + 0) | (__vmem_get__(0, $v + 3) & __vmem_get__(0, $v + 0)) ^ __vmem_get__(0, $v + 4));
+	macro public static function c1(v) return macro untyped (__vmem_get__(0, $v + 7) ^ __vmem_get__(0, $v + 6) | (__vmem_get__(0, $v + 5) & __vmem_get__(0, $v + 6)) ^ __vmem_get__(0, $v + 4) | (__vmem_get__(0, $v + 3) & __vmem_get__(0, $v + 4)) ^ __vmem_get__(0, $v + 2) | (__vmem_get__(0, $v + 1) & __vmem_get__(0, $v + 2)) ^ __vmem_get__(0, $v + 0));
+	macro public static function c2(v) return macro untyped (__vmem_get__(0, $v + 4) ^ __vmem_get__(0, $v + 7) | (__vmem_get__(0, $v + 6) & __vmem_get__(0, $v + 7)) ^ __vmem_get__(0, $v + 5) | (__vmem_get__(0, $v + 0) & __vmem_get__(0, $v + 5)) ^ __vmem_get__(0, $v + 3) | (__vmem_get__(0, $v + 2) & __vmem_get__(0, $v + 3)) ^ __vmem_get__(0, $v + 1));
+	macro public static function c3(v) return macro untyped (__vmem_get__(0, $v + 1) ^ __vmem_get__(0, $v + 0) | (__vmem_get__(0, $v + 3) & __vmem_get__(0, $v + 0)) ^ __vmem_get__(0, $v + 2) | (__vmem_get__(0, $v + 5) & __vmem_get__(0, $v + 2)) ^ __vmem_get__(0, $v + 4) | (__vmem_get__(0, $v + 7) & __vmem_get__(0, $v + 4)) ^ __vmem_get__(0, $v + 6));
+  #else
+	#if js inline #end public static function c0(v:mem.Ptr.AU8):Int return v[5] ^ v[2] | (v[1] & v[2]) ^ v[6] | (v[7] & v[6]) ^ v[0] | (v[3] & v[0]) ^ v[4];
+	#if js inline #end public static function c1(v:mem.Ptr.AU8):Int return v[7] ^ v[6] | (v[5] & v[6]) ^ v[4] | (v[3] & v[4]) ^ v[2] | (v[1] & v[2]) ^ v[0];
+	#if js inline #end public static function c2(v:mem.Ptr.AU8):Int return v[4] ^ v[7] | (v[6] & v[7]) ^ v[5] | (v[0] & v[5]) ^ v[3] | (v[2] & v[3]) ^ v[1];
+	#if js inline #end public static function c3(v:mem.Ptr.AU8):Int return v[1] ^ v[0] | (v[3] & v[0]) ^ v[2] | (v[5] & v[2]) ^ v[4] | (v[7] & v[4]) ^ v[6];
+  #end
+}
