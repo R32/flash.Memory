5804960420956604a1d29f2d94f840f063146bb5
235 <hotup@qq.com>
2017-03-04 17:04:51 +0800

    utf8 update

--- tools/mem/Utf8.hx
+++ tools/mem/Utf8.hx
@@ -2,59 +2,39 @@ package mem;
 
 import mem.Ptr;
 
-/*
- Copyright (c) 2008-2009 Bjoern Hoehrmann <bjoern@hoehrmann.de>
- Permission is hereby granted, free of charge, to any person obtaining a copy
- of this software and associated documentation files (the "Software"), to deal
- in the Software without restriction, including without limitation the rights
- to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- copies of the Software, and to permit persons to whom the Software is
- furnished to do so, subject to the following conditions:
- The above copyright notice and this permission notice shall be included
- in all copies or substantial portions of the Software.
- THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
- OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
- THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
- FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
- DEALINGS IN THE SOFTWARE.
-*/
-
 @:dce @:enum private abstract UTF8Valid(Int) to Int {
 	var UTF8_ACCEPT = 0;
-	var UTF8_REJECT = 1;
+	var UTF8_REJECT = 12;
 }
-#if cpp
-@:nativeGen @:headerCode("#define Utf8hx Utf8hx_obj") @:native("mem.obs.Utf8hx") // for names conflict
-#end
+
+// Copyright (c) 2008-2010 Bjoern Hoehrmann <bjoern@hoehrmann.de>
+// See http://bjoern.hoehrmann.de/utf-8/decoder/dfa/ for details.
 class Utf8 {
-	#if cpp
-	static var utf8d_table(default, null):AU8;
-	#else
+
 	static var utf8d_table(default, null):AU8 = cast Malloc.NUL;
-	#end
 
 	public static function init() {
 		if (utf8d_table != Malloc.NUL) return;
 
-		var data = Mt.utf8DataTo32(); // I32, 100 * 4
+		var data = Mt.utf8DataTo32(); // I32;
 
-		utf8d_table = cast Ram.malloc(400, false);
+		var len = data.length;
+
+		utf8d_table = cast Ram.malloc(len * 4, false);
 
 		var u32:AI32 = cast utf8d_table;
 
-		for (i in 0...100) u32[i] = data[i];
+		for (i in 0...len) u32[i] = data[i];
 	}
 
-	public static function validate(dst:Ptr, byteLength: Int): Bool {
+	public static function validate(src:Ptr, byteLength: Int): Bool {
 		var i = 0, state = 0, utf8d = utf8d_table;
 		var byte:Int, type:Int;
 		while (i < byteLength) {
-			byte = dst[i];
+			byte = src[i];
 			type = utf8d[byte];
 
-			state = utf8d[256 + (state << 4) + type];
+			state = utf8d[256 + state + type];
 
 			if (state == UTF8_REJECT) return false;
 		++ i;
@@ -63,14 +43,14 @@ class Utf8 {
 	}
 
 
-	public static function length(dst:Ptr, byteLength: Int):Int {
+	public static function length(src:Ptr, byteLength: Int):Int {
 		var i = 0, len = 0, state = 0, utf8d = utf8d_table;
 		var byte:Int, type:Int;
 		while (i < byteLength) {
-			byte = dst[i];
+			byte = src[i];
 			type = utf8d[byte];
 
-			state = utf8d[256 + (state << 4) + type];
+			state = utf8d[256 + state + type];
 
 			if (state == UTF8_REJECT)
 				return -1; //throw "Invalid utf8 string";
@@ -81,18 +61,18 @@ class Utf8 {
 		return len;
 	}
 
-	public static function iter(dst:Ptr, byteLength: Int, chars : Int -> Void ):Bool {
+	public static function iter(src:Ptr, byteLength: Int, chars : Int -> Void ):Bool {
 		var i = 0, state = 0, codep = 0, utf8d = utf8d_table;
 		var byte:Int, type:Int;
 		while (i < byteLength) {
-			byte = dst[i];
+			byte = src[i];
 			type = utf8d[byte];
 
 			codep = state != UTF8_ACCEPT ?
 				(byte & 0x3f) | (codep << 6) :
 				(0xff >> type) & (byte);
 
-			state = utf8d[256 + (state << 4) + type];
+			state = utf8d[256 + state + type];
 
 			if (state == UTF8_REJECT)
 				return false;
@@ -103,11 +83,11 @@ class Utf8 {
 		return true;
 	}
 
-	public static function charCodeAt(dst:Ptr, byteLength: Int, index:Int):Int {
+	public static function charCodeAt(src:Ptr, byteLength: Int, index:Int):Int {
 		var i = 0, len = 0, state = 0, codep = 0, utf8d = utf8d_table;
 		var byte:Int, type:Int;
 		while (i < byteLength) {
-			byte = dst[i];
+			byte = src[i];
 			type = utf8d[byte];
 
 			if (len == index) {
@@ -116,7 +96,7 @@ class Utf8 {
 					(0xff >> type) & (byte);
 			}
 
-			state = utf8d[256 + (state << 4) + type];
+			state = utf8d[256 + state + type];
 
 			if (state == UTF8_REJECT) {
 				break; //throw "Invalid utf8 string";
